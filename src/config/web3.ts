11f828ac0e8997774120d481552dcac4b7e2dcde
kyrocypher <kyrocypher@gmail.com>
2024-11-30 23:52:13 +0100

    Create web3.ts

---
+++ src/config/web3.ts
@@ -0,0 +1,25 @@
+import { configureChains, createConfig } from 'wagmi'
+import { mainnet, polygon } from 'wagmi/chains'
+import { publicProvider } from 'wagmi/providers/public'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, polygon],
+  [publicProvider()]
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
