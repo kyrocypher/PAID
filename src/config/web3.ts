314ea1febede1c523ac6a1a5353fc31872436fc1
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:43:39 +0100

    Update web3.ts

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -4,7 +4,7 @@ import { publicProvider } from 'wagmi/providers/public'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
+export const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, polygon],
   [publicProvider()]
 )
