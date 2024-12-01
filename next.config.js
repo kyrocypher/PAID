fc8ee513fb6f3f61631672e719e0a3a3b366cd00
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:37:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,6 +20,19 @@ const nextConfig = {
     }
     return config
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+          }
+        ],
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
