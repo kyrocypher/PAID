2ef8047c987ee83c54d4784a4701178b6f54cefd
kyrocypher <kyrocypher@gmail.com>
2024-12-01 01:28:37 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
-import Layout from '@/components/layout/Layout'
-import Web3Provider from '@/components/providers/Web3Provider'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Web3Provider>
-      <Layout>
-        <Component {...pageProps} />
-        <Analytics />
-      </Layout>
-    </Web3Provider>
-  )
+  return <Component {...pageProps} />
 }
