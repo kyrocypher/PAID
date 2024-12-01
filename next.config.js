4f7c3be9a65ede89d29a7d036551b1e44d1bf1d3
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:02:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,9 +6,16 @@ const nextConfig = {
     domains: ['avatars.dicebear.com'],
   },
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false,
+      crypto: false,
+      stream: false,
+      http: false,
+      https: false,
+      zlib: false,
+      path: false,
+      os: false
+    }
     return config
-  },
-}
-
-module.exports = nextConfig
