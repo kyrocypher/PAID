0f64046e8053251004ad6a057b7554cd9094bc0c
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:53:39 +0100

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,13 @@
-import { ReactNode } from 'react'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 
-interface LayoutProps {
-  children: ReactNode
-}
-
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
       <Navbar />
       <div className="flex">
         <Sidebar />
-        <main className="flex-1 p-6 lg:p-8">{children}</main>
+        <main className="flex-1 p-8">{children}</main>
       </div>
     </div>
   )
