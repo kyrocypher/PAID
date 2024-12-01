a94ddd60fc30e0406ff5485f361563421f76b4d4
kyrocypher <kyrocypher@gmail.com>
2024-12-01 01:36:42 +0100

    Update _app.tsx

--- src/components/layout/src/pages/src/pages/_app.tsx
+++ src/components/layout/src/pages/src/pages/_app.tsx
@@ -1,15 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
-import Layout from '@/components/layout/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      <Analytics />
-    </>
-  )
+  return <Component {...pageProps} />
 }
