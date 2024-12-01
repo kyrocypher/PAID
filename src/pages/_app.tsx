7356534ebb8ad48bd313b8b8e2903d78cffc6b41
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:49:04 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import Layout from '@/components/layout/Layout'
-import { WagmiConfig } from 'wagmi'
-import { config } from '@/config/web3'
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import { chains } from '@/config/web3'
+import Web3Provider from '@/components/providers/Web3Provider'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig config={config}>
-      <RainbowKitProvider chains={chains}>
-        <Layout>
-          <Component {...pageProps} />
-          <Analytics />
-        </Layout>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <Web3Provider>
+      <Layout>
+        <Component {...pageProps} />
+        <Analytics />
+      </Layout>
+    </Web3Provider>
   )
 }
