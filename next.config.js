dbea0e04f33837521cfb60727c0bc5cc590a49e2
kyrocypher <kyrocypher@gmail.com>
2024-12-01 01:58:59 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['avatars.dicebear.com'],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    return config
+  },
 }
 
 module.exports = nextConfig
