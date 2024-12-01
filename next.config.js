29d3f06113ff645034b749cc3785406b4b87155d
kyrocypher <kyrocypher@gmail.com>
2024-12-01 01:32:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['avatars.dicebear.com'],
-  },
 }
 
 module.exports = nextConfig
