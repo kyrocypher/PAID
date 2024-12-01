46afab1c29efc8f7fd8eba9dfdfbaf36c1d4fdf7
kyrocypher <kyrocypher@gmail.com>
2024-12-01 02:34:22 +0100

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,3 @@
-export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string
-export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID || '1')
+export const CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as `0x${string}`
+export const CHAIN_ID = Number(process.env.NEXT_PUBLIC_CHAIN_ID) || 1
+export const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || ''
